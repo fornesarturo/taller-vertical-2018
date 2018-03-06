97eb851a2e04ac6e71b2f4ba52b443e037e4baa3
fornesarturo <fornesarturo@gmail.com>
2018-03-06 17:43:16 -0600

    Change scene

--- TallerVertical2018/Assets/Scripts/ChangeLevel.cs
+++ TallerVertical2018/Assets/Scripts/ChangeLevel.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChangeLevel : MonoBehaviour {
 
+	public Slider slider;
 	// Use this for initialization
 	private string sceneToLoad;
 	void Start () {
 		sceneToLoad = PlayerPrefs.GetString ("NextSceneToLoad");
+		slider.value = 0f;
 		StartCoroutine ("LoadNewScene");
 	}
 	
@@ -18,11 +21,22 @@ public class ChangeLevel : MonoBehaviour {
 	}
 
 	IEnumerator LoadNewScene() {
-
-		yield return new WaitForSeconds (2f);
-
 		AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
-
+		async.allowSceneActivation = false;
+		while (async.progress <= 0.89f) {
+			slider.value = async.progress;
+			if (async.progress >= 0.88f) {
+				slider.value = 1f;
+			}
+		}
+		async.allowSceneActivation = true;
 		yield return async;
+
+//		float seconds = 0f;
+//		while (seconds <= 10f) {
+//			slider.value = seconds / 10f;
+//			yield return new WaitForSeconds (1f);
+//			seconds += 1f;
+//		}
 	}
 }
