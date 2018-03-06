75fd78e75c9ca8910cbdee77e4a99fb759811e0f
geraJuarez <a00226860@itesm.mx>
2018-03-06 12:36:01 -0600

    door interaction

---
+++ TallerVertical2018/Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	//public GameObject player;
+	// attribute to taht defines the scene to load
+	public string myName = "hola";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void enterToHouse() {
+		
+	}
+
+	void OnCollisionEnter (Collision col) {
+	}
+		
+}
---
+++ TallerVertical2018/Assets/Scripts/PlayerDoorInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDoorInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnControllerColliderHit (ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "Door") {
+			// DO something
+			//hit.gameObject.GetComponent<DoorController>().myName 
+			Debug.Log(hit.gameObject.GetComponent<DoorController>().myName );
+		}
+	}
+}
