5ded0881dc1c494591d175ba8a81624b5c0871d9
unknown <juanetamayog@gmail.com>
2018-03-06 17:48:44 -0600

    UI interface updates and conversation script

---
+++ TallerVertical2018/Assets/UI/ConversationManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class ConversationManager : MonoBehaviour {
+
+    public Text animalName;
+    public Text conversation;
+
+    public GameObject canvas;
+
+    public FileInfo theSourceFile;
+    protected StreamReader reader = null;
+
+    // Use this for initialization
+    void Start () {
+
+        canvas.SetActive(false);
+        theSourceFile = new FileInfo("ConvoDePrueba.txt");
+        reader = theSourceFile.OpenText();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateText();
+    }
+
+    void updateText() {
+        if (Input.GetKeyDown("space")) {
+            if (reader.EndOfStream)
+                canvas.SetActive(false);
+            if (!canvas.activeInHierarchy & !reader.EndOfStream)
+                canvas.SetActive(true);
+            animalName.text = reader.ReadLine();
+            conversation.text = reader.ReadLine();
+
+        }
+    }
+}
---
+++ TallerVertical2018/Assets/UI/UpdateUi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUi : MonoBehaviour {
+
+    public Text smellVisionStatus;
+    public Text currentLocation;
+
+    bool smellVision;
+
+	// Use this for initialization
+	void Start () {
+        smellVision = false;
+        smellVisionStatus.text = "DEACTIVATED";
+        smellVisionStatus.color = Color.red;
+        currentLocation.text = "My house";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        activateSmellVision();
+	}
+
+    void activateSmellVision() {
+        if (Input.GetKeyDown("space"))
+        {
+            if (!smellVision) {
+                smellVisionStatus.text = "ACTIVATED";
+                smellVisionStatus.color = Color.green;
+                smellVision = true;
+            }
+            else {
+                smellVisionStatus.text = "DEACTIVATED";
+                smellVisionStatus.color = Color.red;
+                smellVision = false;
+            }
+        }
+    }
+}
