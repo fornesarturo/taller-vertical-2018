fcf2c021fa862fe20e6a2498f8b7b326a12b7b58
fornesarturo <fornesarturo@gmail.com>
2018-03-06 12:35:17 -0600

    Gazed at

---
+++ TallerVertical2018/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+	private bool selected;
+	public Material selectedMaterial;
+	public Material normalMaterial;
+	private Renderer renderer;
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetSelected(bool selected) {
+		this.selected = selected;
+		renderer.material = selected ? selectedMaterial: normalMaterial;
+	}
+}
