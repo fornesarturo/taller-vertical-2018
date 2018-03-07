a11b3b67ede8b792897071979d6ca3e6fdd282e0
geraJuarez <a00226860@itesm.mx>
2018-03-06 23:45:04 -0600

    Scene to show case file

---
+++ TallerVertical2018/Assets/Scripts/CaseFileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaseFileController : MonoBehaviour {
+
+	public Transform canvas;
+	public Transform player;
+	public Text cluesBody;
+
+	// Use this for initialization
+	void Start () {
+		showCaseFiles ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void showCaseFiles() {
+		canvas.gameObject.SetActive (true);
+		canvas.position = player.GetChild (0).position + player.GetChild (0).forward * 2f;
+		canvas.rotation = player.GetChild (0).rotation;
+		//string body = PlayerPrefs.GetString ("cluesbody1");
+		this.cluesBody.text = "Hola - Hello - Aloha \nPatos y Conejos\n Arturo es la toxina";
+	}
+
+	public void showCaseFile() {
+		Debug.Log ("case file");
+	}
+}
