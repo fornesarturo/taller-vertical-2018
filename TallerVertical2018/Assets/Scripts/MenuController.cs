72500daba9a84bbe2a575f090f2771552cb64dba
fornesarturo <fornesarturo@gmail.com>
2018-03-08 16:11:30 -0600

    Starter Screen that deletes PlayerPrefs

---
+++ TallerVertical2018/Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController: MonoBehaviour {
+
+	void Awake() {
+		
+	}
+
+	void Start () {
+		PlayerPrefs.DeleteAll ();
+		StartCoroutine ("goToCity");
+	}
+
+	void Update () {
+		
+	}
+
+	IEnumerator goToCity() {
+		float seconds = 0f;
+		while (true) {
+			if (seconds < 1f) {
+				seconds += 1f;
+			} else {
+				PlayerPrefs.SetString ("NextSceneToLoad", "City");
+				SceneManager.LoadScene ("LoadingScreen", LoadSceneMode.Single);
+			}
+			yield return new WaitForSecondsRealtime (1f);
+		}
+	}
+}
