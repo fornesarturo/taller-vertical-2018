cf310d88e2c3caf9ed9c60801f90475e65b9f43e
geraJuarez <a00226860@itesm.mx>
2018-03-08 10:07:29 -0600

    Organize scripts in folders

--- TallerVertical2018/Assets/Scripts/ResetPrefabs.cs
+++ TallerVertical2018/Assets/Scripts/ResetPrefabs.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ResetPrefabs : MonoBehaviour {
 
+	void Awake() {
+		PlayerPrefs.SetInt ("ObjectCollected", 0);
+	}
+
 	// Use this for initialization
 	void Start () {
-		PlayerPrefs.SetInt ("ObjectCollected", 0);
+		//PlayerPrefs.SetInt ("ObjectCollected", 0);
 	}
 	
 	// Update is called once per frame
