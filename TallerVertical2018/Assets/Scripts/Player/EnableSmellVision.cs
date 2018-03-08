ea82a28acfa1dec96a16c92b380b5cd8e3257ff1
geraJuarez <a00226860@itesm.mx>
2018-03-08 13:29:57 -0600

    smell vision after talking to the cat

---
+++ TallerVertical2018/Assets/Scripts/Player/EnableSmellVision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSmellVision : MonoBehaviour {
+
+	private Transform Player;
+
+	void Awake () {
+
+		Player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void enableSmell() {
+		Player.gameObject.GetComponent<SmellVision> ().enabled = true;
+		Player.position = this.transform.position;
+		Player.rotation = this.transform.rotation;
+		Destroy (this.gameObject);
+	}
+}
