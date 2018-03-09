d33e646aa2ca84fc8ac143de66b9a339ef58f24d
chuyalvarez <jesusaagomez@hotmail.com>
2018-03-08 18:08:28 -0600

    added switching between dialogs and transition to case files

---
+++ TallerVertical2018/Assets/SecondDoorSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondDoorSwitch : MonoBehaviour {
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void EnableDoor(){
+		door.GetComponent<DoorController> ().enabled = true;
+		this.gameObject.SetActive (false);
+	}
+}
