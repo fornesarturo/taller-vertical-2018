9a155c9ac5150da04e149d6f80bf3cf9dc1ae4d1
geraJuarez <a00226860@itesm.mx>
2018-03-08 18:21:19 -0600

    Loading screen now loads

--- TallerVertical2018/Assets/Scripts/ChangeLevel.cs
+++ TallerVertical2018/Assets/Scripts/ChangeLevel.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 public class ChangeLevel : MonoBehaviour {
 
 	public Slider slider;
+	[SerializeField] private Text textLoading;
+	private AsyncOperation async;
+
 	// Use this for initialization
 	private string sceneToLoad;
 	void Start () {
@@ -21,16 +24,51 @@ public class ChangeLevel : MonoBehaviour {
 	}
 
 	IEnumerator LoadNewScene() {
-		AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+		yield return null;
+		async = SceneManager.LoadSceneAsync(sceneToLoad);
 		async.allowSceneActivation = false;
-		while (async.progress <= 0.89f) {
-			slider.value = async.progress;
-			if (async.progress >= 0.88f) {
-				slider.value = 1f;
+
+		while (!async.isDone) {
+
+			float progess = Mathf.Clamp01 (async.progress / 0.9f);
+			slider.value = progess;
+			if (async.progress == 0.9f) {
+				textLoading.text = "Picale Compa";
+				if (Input.anyKey) {					
+					async.allowSceneActivation = true;	
+				}
 			}
+			yield return null;
 		}
-		async.allowSceneActivation = true;
-		yield return async;
+
+
+//		async = SceneManager.LoadSceneAsync(sceneToLoad);
+//		async.allowSceneActivation = false;
+//		while (!async.isDone) {
+//			slider.value = async.progress;
+//			if (async.progress == 0.9f) {
+//				slider.value = 1f;
+//				async.allowSceneActivation = true;
+//			}
+//			yield return null;
+//		}
+//
+//		async.allowSceneActivation = true;
+		//yield return null;
+//		while (async.progress <= 0.89f) {
+//			slider.value = async.progress;
+//			Debug.Log ("slider1: " + slider.value);
+//			Debug.Log ("progress: " + async.progress);
+//			if (async.progress >= 0.96f) {
+//				slider.value = 1f;
+//			}
+//			Debug.Log ("slider2: " + slider.value);
+//		}
+//		async.allowSceneActivation = true;
+//		while (!async.isDone) {
+//			slider.value = async.progress;
+//		}
+		//yield return async;
 
 //		float seconds = 0f;
 //		while (seconds <= 10f) {
